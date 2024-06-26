714d0d476036dadd8deb3426c6fe89542bccdac2
AbhishikthIIITS <abhiabhishikth18@gmail.com>
2024-06-26 20:41:27 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div className="bg-orange-500 py-10">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <Link
+                    to="/"
+                    className="text-3xl text-white font-bold tracking-tight"
+                >
+                    © HungerHalt
+                </Link>
+                <span className="text-white font-bold tracking-tight flex gap-4">
+                    <span>Privacy Policy</span>
+                    <span>About Us</span>
+                    <span>Terms of Service</span>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
