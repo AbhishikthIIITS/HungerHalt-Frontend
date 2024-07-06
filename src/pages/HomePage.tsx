6fbe0c388aa47ab413039b2f3ca3f1212630853c
AbhishikthIIITS <abhiabhishikth18@gmail.com>
2024-07-06 12:08:56 +0530

    outlets added  2/n

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,37 +6,62 @@ import { useNavigate } from "react-router-dom";
 const HomePage = () => {
     const navigate = useNavigate();
 
-    const handleSearchSubmit = (searchFormValues: SearchForm)=> {
+    const handleSearchSubmit = (searchFormValues: SearchForm) => {
         navigate({
             pathname: `/search/${searchFormValues.searchQuery}`,
         })
     }
-  return(
-    <div className="md:px-32 flex flex-col gap-12">
-        <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-            <h1 className="text-4xl font-bold tracking-tight text-orange-600">
-                Treat your taste buds to a feast of flavors today!
-            </h1>
-            <span className="text-xl">Halt your hunger with just one click!</span>
-            <SearchBar 
-                placeHolder="Search by City or Town"
-                onSubmit={handleSearchSubmit}
-            />
-        </div>
-        <div className="grid md:grid-cols-2 gap-5">
-            <img src={landingImage} />
-            <div className="flex flex-col intems-center justify-center gap-4 text-center">
-                <span className="font-bold text-3xl tracking-tighter">
-                    Order takeaway even faster!
-                </span>
-                <span>
-                    Download the HungerHalt App for faster ordering and personalised recommendations
-                </span>
-                <img src={appDownload} className="block mx-auto w-1/2"/>
+    return (
+        <div className="md:px-32 flex flex-col gap-12">
+            <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+                <h1 className="text-4xl font-bold tracking-tight text-orange-600">
+                    Treat your taste buds to a feast of flavors today!
+                </h1>
+                <span className="text-xl">Halt your hunger with just one click!</span>
+                <SearchBar
+                    placeHolder="Search by City or Town"
+                    onSubmit={handleSearchSubmit}
+                />
+            </div><div className="bg-white rounded-lg shadow-md bg-orange py-8 flex flex-col gap-5 text-center mx-5">
+                <h3 className="text-3xl font-bold text-orange-500">Our Current Outlets:</h3>
+                <div className="flex justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-4  text-xl text-gray-700 w-full">
+                        <div className="hidden md:flex"></div> {/* Placeholder for spacing on small screens */}
+                        <div className="flex flex-col items-center">
+                            <ul className="list-disc">
+                                <li>Kakinada</li>
+                                <li>Vijayawada</li>
+                                <li>Vizag</li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <ul className="list-disc list-outside">
+                                <li>Nellore</li>
+                                <li>Guntur</li>
+                                <li>Tada</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div className="grid md:grid-cols-2 gap-5">
+                <img src={landingImage} />
+                <div className="flex flex-col intems-center justify-center gap-4 text-center">
+                    <span className="font-bold text-3xl tracking-tighter">
+                        Order takeaway even faster!
+                    </span>
+                    <span>
+                        Download the HungerHalt App for faster ordering and personalised recommendations
+                    </span>
+                    <img src={appDownload} className="block mx-auto w-1/2" />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default HomePage;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,10 +2,23 @@ import { useSearchRestaurants } from "@/api/RestaurantApi";
 import SearchResultInfo from "@/components/SearchResultInfo";
 import SearchResultCard from "@/components/SearchResultCard";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+
+export type SearchState = {
+  searchQuery: string;
+}
 
 const SearchPage = () => {
   const { city } = useParams();
+  // const { SearchState, setSearchState} = useState<SearchState>({
+  //   searchQuery: ""
+  // })
   const { results, isLoading } = useSearchRestaurants(city);
+
+  // const setSearchQuery = (searchFormdata: SearchForm) => {
+
+  // }
 
   if(isLoading) {
     return <span>Loading....</span>
@@ -20,6 +33,7 @@ const SearchPage = () => {
         Insert cuisines here :)
       </div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar onSubmit={setSearchQuery}/>
         <SearchResultInfo total={results.pagination.total} city={city}/>
         {results.data.map((restaurant)=>(
           <SearchResultCard restaurant={restaurant}/>
