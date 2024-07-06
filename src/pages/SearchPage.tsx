ce1be882076e3247fe5b410b52083439879f432c
AbhishikthIIITS <abhiabhishikth18@gmail.com>
2024-07-06 12:16:53 +0530

    outlets added  4/n

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,20 +5,15 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 
-export type SearchState = {
-  searchQuery: string;
-}
+
 
 const SearchPage = () => {
   const { city } = useParams();
-  // const { SearchState, setSearchState} = useState<SearchState>({
-  //   searchQuery: ""
-  // })
   const { results, isLoading } = useSearchRestaurants(city);
 
-  // const setSearchQuery = (searchFormdata: SearchForm) => {
+  const setSearchQuery = (searchFormdata: SearchForm) => {
 
-  // }
+  }
 
   if(isLoading) {
     return <span>Loading....</span>
@@ -33,7 +28,6 @@ const SearchPage = () => {
         Insert cuisines here :)
       </div>
       <div id="main-content" className="flex flex-col gap-5">
-        <SearchBar onSubmit={setSearchQuery}/>
         <SearchResultInfo total={results.pagination.total} city={city}/>
         {results.data.map((restaurant)=>(
           <SearchResultCard restaurant={restaurant}/>
